752eb10e42e00c9acf00a688fa625acf76d22916
marcealita25 <conialto@gmail.com>
2025-01-17 18:21:41 +0100

    cambios viernes

---
+++ src/funcionesjava/src/PruebaUtilidadesjava.java
@@ -0,0 +1,35 @@
+package funcionesjava.src;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class PruebaUtilidadesjava {
+	
+	public static void main(String[] args) {
+		
+		int edad = Utilidadesjava.pideDatoNumerico("Introduce tu edad");
+		
+		int opcion= Utilidadesjava.pideDatoNumerico("Introduce una opcion");
+		
+		int anio = Utilidadesjava.pideDatoNumerico("Introduce tu año de nacimiento");
+        
+        String nombre = Utilidadesjava.pideDatoCadena("Introduce tu nombre");
+        
+        System.out.println(nombre.toUpperCase());
+
+
+        String [] opcionesMenu = {"1.- opcion 1", "2. opcion 2"};
+        
+
+	       
+    }
+
+    
+		
+
+	   
+	
+	   
+
+
+
+}
---
+++ src/funcionesjava/src/Utilidadesjava.java
@@ -0,0 +1,62 @@
+package funcionesjava.src;
+
+import java.util.Scanner;
+
+public class Utilidadesjava {
+
+    public static int pideDatoNumerico(String texto) {
+	 
+	System.out.println(texto);
+	 
+	 Scanner scan = new Scanner(System.in);
+	 
+	 int numero = scan.nextInt();
+	 
+	 return numero;
+	 
+	 }
+	public static String pideDatoCadena(int texto) {
+		
+		System.out.println(texto);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		String cadena = scan.nextLine();
+		
+		return cadena;
+	}
+	 
+     public static void pintaMenu(String[] menuArray) {
+
+        for (String opcion : menuArray){
+
+            System.out.println(opcion);
+        }
+        System.out.println("Introduce una opcion");
+        
+     }
+     
+
+     public static void pintaMenu(String[] menuArray, String texto) {
+
+        for (String opcion : menuArray){
+
+            System.out.println(opcion);
+        }
+        System.out.println("texto");
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
